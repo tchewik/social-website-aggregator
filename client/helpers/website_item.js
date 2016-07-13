a0574898b5a94f21e0bb1d32f344a001ed45a82d
tchewik <elenachistov@gmail.com>
2016-07-14 02:05:38 +0300

    frontend features

--- client/helpers/website_item.js
+++ client/helpers/website_item.js
@@ -1,21 +1,18 @@
 Template.websiteItem.events({
     "click .js-upvote":function(event){
-        // example of how you can access the id for the website in the database
-        // (this is the data context for the template)
         var website_id = this._id;
         Meteor.call('vote', website_id, 1);
-
         return false;// prevent the button from reloading the page
     },
     "click .js-downvote":function(event){
-
-        // example of how you can access the id for the website in the database
-        // (this is the data context for the template)
         var website_id = this._id;
         Meteor.call('vote', website_id, -1);
-
-        // put the code in here to remove a vote from a website!
-
         return false;// prevent the button from reloading the page
+    }
+});
+
+Template.websiteItem.helpers({
+    date: function(){
+        return Websites.findOne({'_id': this._id}).createdOn.toDateString();
     }
 });
