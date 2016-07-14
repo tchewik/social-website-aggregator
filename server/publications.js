2f8c72a631fc382c256d706c555ade9bd51564d9
tchewik <elenachistov@gmail.com>
2016-07-14 19:55:00 +0300

    frontend for home page

--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,11 @@
 Meteor.publish('websites', function(){
     return Websites.find({}, {sort: {'rating': -1}});
 });
+
+Meteor.publish('singleUrl', function(id){
+    return Websites.find({'_id':id},{});
+});
+
+Meteor.publish('comments', function (id) {
+    return Comments.find({'urlId':id});
+});
