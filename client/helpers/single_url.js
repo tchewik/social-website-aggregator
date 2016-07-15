5619e848d0bb48ded86a3858fe2f209129d28f57
tchewik <elenachistov@gmail.com>
2016-07-15 17:15:12 +0300

    implemented find of page picture (l.a. server/https_methos.js)

--- client/helpers/single_url.js
+++ client/helpers/single_url.js
@@ -1,10 +1,27 @@
 Template.singleUrl.helpers({
     comments: function () {
         return Comments.find({'urlId': this._id}, {sort: {'date': -1}});
-    },
-    siteImg: function () {
-        var re = /(https?:\/\/[a-z0-9]+(\.[a-z0-9]+)+)/ig;
-        var url_to_pic = this.url.match(re)[0] + "/favicon.ico";
-        return url_to_pic;
     }
 });
+
+Template.singleUrl.rendered = function () {
+    console.log("siteImg form single_url says:");
+    url = this.data.url;
+    setTimeout(function () {
+        Meteor.call("findPic", url, function(error, response){
+            if (error) {
+                $('#site-img').attr("src", "img_not_found.jpg");
+            }
+            if (response) {
+                $('#site-img').attr("src", response);
+                //$('#site-svg').attr("")
+                // if function returns .svg, it is black. let's change it's color to eadcf9 (white)
+                if (response.match(/\.svg$/i)) {
+                    console.log(".svg!");
+                    $('#site-img').attr("style", "background-color: #eadcf9;");
+                }
+            }
+        });
+        return true;
+    }, 0.5);
+};
--- server/http_methods.js
+++ server/http_methods.js
@@ -9,5 +9,40 @@ Meteor.methods({
         } catch(e) {
             console.log(">>>>"+url+"  description is not found");
         }
+    },
+    "findPic": function(url) {
+        var re = /(https?:\/\/[a-z0-9]+(\.[a-z0-9]+)+)/ig;
+        url = ''+url.match(re);
+
+        var content = HTTP.get(url.match(re)[0]).content;
+        try {
+            // check if icon for page is in <link rel="icon" href="..."> (like in coursera.org)
+            re = /<link rel="icon"[^>]+href=\"([^ \"\']+)\"/i;
+            var matches = content.match(re);
+            if (matches) {
+                url_to_pic = matches[1];
+                console.log(url_to_pic);
+                if (url_to_pic.match(/^\/.*/)) {
+                    return url+url_to_pic;
+                } else {
+                    return url_to_pic;
+                }
+            }
+
+            // check if icon for page is in <link rel="...-icon" href="..."> (like in twitter.com)
+            re = /<link rel=\"[^">]+-icon\"[^>]+href=\"([^ \"\']+)\"/i;
+            var matches = content.match(re);
+            if (matches) {
+                url_to_pic = matches[1];
+                console.log(url_to_pic);
+                return url_to_pic;
+            }
+
+            // just take .../favicon.ico (like in google.com)
+            throw(e);
+        } catch(e) {
+            console.log(">>> picture is not found for url "+url);
+            return url+'/favicon.ico';
+        }
     }
 });
